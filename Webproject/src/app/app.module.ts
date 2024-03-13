af6f13ebe863a17ce3c60892987a4cf642fe9c2b
CLOD002 <maximilianfelix.clodius@bertelsmann.de>
2024-03-13 14:20:46 +0100

    added angular materials

--- Webproject/src/app/app.module.ts
+++ Webproject/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppComponent } from './app.component';
-import { BoardHeaderComponent } from './board-header/board-header.component';
+import { BoardHeaderComponent } from './components/board-header/board-header.component';
 
 @NgModule({
   declarations: [
