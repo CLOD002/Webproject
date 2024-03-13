e4c1954a78582254cb764af06e2eae503eceb6e2
CLOD002 <maximilianfelix.clodius@bertelsmann.de>
2024-03-13 09:54:41 +0100

    added components folder and header component

--- Webproject/src/app/app.module.ts
+++ Webproject/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BoardHeaderComponent } from './board-header/board-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardHeaderComponent
   ],
   imports: [
     BrowserModule
