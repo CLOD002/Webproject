74053feb0edf12c503b3c5f8815153af37b9fa6f
CLOD002 <maximilianfelix.clodius@bertelsmann.de>
2024-07-10 10:44:14 +0200

    added routes for profile and login component

--- Webproject/src/app/app-routing.module.ts
+++ Webproject/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { BoardTemplateComponent } from './components/boards/backlog-items-board/backlog-items-board.component';
 import { FeatureBoardComponent } from './components/boards/feature-board/feature-board.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const appRoutes: Routes = [
     {path: 'backlog-board', component: BoardTemplateComponent},
     {path: 'feature-board', component: FeatureBoardComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'profile', component: ProfileComponent},
 ];
 
 @NgModule({
