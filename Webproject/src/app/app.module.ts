ffe9584a862b12879a3c7d43cc1d8d1e77ad67ff
CLOD002 <maximilianfelix.clodius@bertelsmann.de>
2024-07-10 09:55:11 +0200

    fixed problems with adding second board

--- Webproject/src/app/app.module.ts
+++ Webproject/src/app/app.module.ts
@@ -13,10 +13,10 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { BoardViewComponent } from './components/board-view/board-view.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BoardTemplateComponent } from './components/backlog-items-board/backlog-items-board.component';
+import { BoardTemplateComponent } from './components/boards/backlog-items-board/backlog-items-board.component';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import { RouterModule } from '@angular/router';
-import { FeatureBoardComponent } from './feature-board/feature-board.component';
+import { FeatureBoardComponent } from './components/boards/feature-board/feature-board.component';
 
 @NgModule({
   declarations: [
--- Webproject/src/app/components/boards/feature-board/feature-board.component.ts
+++ Webproject/src/app/components/boards/feature-board/feature-board.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-feature-board',
@@ -6,5 +13,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./feature-board.component.scss']
 })
 export class FeatureBoardComponent {
+  new = ['Item1', 'Item2', 'Item3'];
 
+  inProgress = ['Item7', 'Item8', 'Item9'];
+
+  testReady = ['ItemX', 'ItemXI', 'ItemXII'];
+
+  done = ['ItemXIII', 'ItemXIV', 'ItemXV'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
