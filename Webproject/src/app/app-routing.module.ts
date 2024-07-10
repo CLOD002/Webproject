f0b8b5d717e618c1000a9bb22e339c63dc528e99
CLOD002 <maximilianfelix.clodius@bertelsmann.de>
2024-07-10 09:58:26 +0200

    added board routes

--- Webproject/src/app/app-routing.module.ts
+++ Webproject/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
+import { BoardTemplateComponent } from './components/boards/backlog-items-board/backlog-items-board.component';
+import { FeatureBoardComponent } from './components/boards/feature-board/feature-board.component';
 
 const appRoutes: Routes = [
-    
+    {path: 'backlog-board', component: BoardTemplateComponent},
+    {path: 'feature-board', component: FeatureBoardComponent},
 ];
 
 @NgModule({
