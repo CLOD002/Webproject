3f258243f32d691bd70189d824e4eabfe23e96bf
CLOD002 <maximilianfelix.clodius@bertelsmann.de>
2024-07-11 13:55:22 +0200

    restructered folders in project

--- Webproject/src/app/app.module.ts
+++ Webproject/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {MatCardModule} from '@angular/material/card';
 
 import { AppComponent } from './app.component';
-import { BoardHeaderComponent } from './components/board-header/board-header.component';
+import { BoardHeaderComponent } from './components/boards/board-header/board-header.component';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import { MenuComponent } from './components/menu/menu.component';
@@ -11,7 +11,7 @@ import {MatDividerModule} from '@angular/material/divider';
 import {MatSelectModule} from '@angular/material/select';
 import {MatButtonModule} from '@angular/material/button';
 import {MatExpansionModule} from '@angular/material/expansion';
-import { BoardViewComponent } from './components/board-view/board-view.component';
+import { BoardViewComponent } from './components/boards/board-view/board-view.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BoardTemplateComponent } from './components/boards/backlog-items-board/backlog-items-board.component';
 import {DragDropModule} from '@angular/cdk/drag-drop';
